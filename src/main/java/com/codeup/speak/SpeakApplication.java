eaa2d5722318a746812d34b7b51384c757205a10
tcolejimenez <taylor.cole.jimenez@gmail.com>
2018-10-29 10:03:09 -0500

    setting up project for spring blog house - speak

---
+++ src/main/java/com/codeup/speak/SpeakApplication.java
@@ -0,0 +1,12 @@
+package com.codeup.speak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeakApplication.class, args);
+    }
+}
